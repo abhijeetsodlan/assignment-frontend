a309850d753ace1aab741414cb78b13b36d6886c
Abhijeet Sodlan <abhijeetsodlan7@gmail.com>
2026-04-01 22:18:30 +0530

    images issue fixed

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
-﻿/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com'
+        hostname: '**'
       },
       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc'
+        protocol: 'http',
+        hostname: '**'
       }
     ]
   }
 };
 
 module.exports = nextConfig;
-
-
